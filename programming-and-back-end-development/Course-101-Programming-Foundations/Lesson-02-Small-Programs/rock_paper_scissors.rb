ca2924c38c996f3b4fec9e442ef4fc103f96cc63
theOtherRan <mrxmemo@yahoo.com>
2016-05-07 22:23:04 -0800

    Added RPS bonus features:   * add two more options: Lizard and Spock   * modified program to have user input be first character instead of full word   * keep score of number of wins for player and computer

--- programming-and-back-end-development/Course-101-Programming-Foundations/Lesson-02-Small-Programs/rock_paper_scissors.rb
+++ programming-and-back-end-development/Course-101-Programming-Foundations/Lesson-02-Small-Programs/rock_paper_scissors.rb
@@ -1,47 +1,88 @@
-VALID_CHOICES = %w(rock paper scissors)
+WINNING_RULES = {
+  'rock' => %w(scissors lizard),
+  'paper' => %w(rock spock),
+  'scissors' => %w(paper lizard),
+  'lizard' => %w(paper spock),
+  'spock' => %w(rock scissors)
+}.freeze
+
+SHORTCUT_RULES = { "r" => "rock", "p" => "paper", "s" => "scissors", "v" => "spock", "l" => "lizard" }.freeze
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def clear_screen
+  system('clear') || system('cls')
+end
+
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  WINNING_RULES[first].include?(second)
 end
 
-def display_results(player, computer)
-  if win?(player, computer)
-    prompt("You won!")
-  elsif win?(computer, player)
-    prompt("Computer won!")
+def display_results(user_wins, computer_wins)
+  if user_wins > computer_wins
+    prompt("You won the game!")
+  elsif computer_wins > user_wins
+    prompt("Computer won the game!")
   else
-    prompt("It's a tie!")
+    prompt("It's a tie.")
   end
 end
 
-loop do
-  player = ''
-  loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    player = Kernel.gets().chomp().downcase()
+def valid_answer?(answer)
+  answer.downcase == 'y' || answer.downcase == 'n'
+end
 
-    if VALID_CHOICES.include?(player)
-      break
-    else
-      prompt("That's not a valid choice.")
+loop do # main loop
+  user_choice_key = ''
+  user_wins = 0
+  computer_wins = 0
+  winner_found = false
+  round = 1 # keeping track of rounds
+  until winner_found == true
+    clear_screen()
+    prompt("--------------------Round #{round}--------------------")
+    loop do
+      prompt("Enter your choice:")
+      prompt("r for rock, p for paper, s for scissors, l for lizard, or v for spock")
+      user_choice_key = Kernel.gets().chomp()
+      break if SHORTCUT_RULES.key?(user_choice_key.downcase)
     end
+
+    user_choice = SHORTCUT_RULES[user_choice_key]
+    computer_choice = WINNING_RULES.keys.sample
+
+    prompt("You chose #{user_choice}; Computer chose #{computer_choice}")
+
+    if win?(user_choice, computer_choice)
+      prompt("You won this round!")
+      user_wins += 1
+    elsif win?(computer_choice, user_choice)
+      prompt("Computer won this round!")
+      computer_wins += 1
+    else
+      prompt("It's a tie.")
+    end
+
+    prompt("Current Score is User: #{user_wins} || Computer: #{computer_wins}")
+    prompt("Press enter to continue... ")
+    Kernel.gets # pause the screen
+
+    round += 1
+    winner_found = true if user_wins == 5 || computer_wins == 5
   end
 
-  computer = VALID_CHOICES.sample
+  display_results(user_wins, computer_wins)
 
-  Kernel.puts("You chose: #{player}; Computer chose: #{computer}")
+  answer = ''
+  loop do
+    prompt("Do you want to play again? Enter Y or N")
+    answer = Kernel.gets().chomp()
+    break if valid_answer?(answer)
+  end
 
-  display_results(player, computer)
-
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  break unless answer.downcase == 'y'
 end
 
 prompt("Thank you for playing. Good bye!")
