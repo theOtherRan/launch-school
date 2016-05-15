b95ee7036e0aede105bcecc42cc4645cd310d901
theOtherRan <mrxmemo@yahoo.com>
2016-05-14 21:22:39 -0800

    Built Tic Tac Toe

---
+++ programming-and-back-end-development/Course-101-Programming-Foundations/Lesson-04-Slightly-Larger-Programs/tictactoe.rb
@@ -0,0 +1,108 @@
+require 'pry'
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def display_board(board)
+  system 'clear'
+  puts "You're a #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}."
+  puts ""
+  puts "     |     |"
+  puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  new_board
+end
+
+def empty_squares(board)
+  board.keys.select{|num| board[num] == INITIAL_MARKER}
+end
+
+def player_places_piece!(board)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(board).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(board).include?(square)
+      prompt "Sorry, that's not a valid choice."
+  end
+  board[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(board)
+  square = empty_squares(board).sample
+  board[square] = COMPUTER_MARKER
+end
+
+def board_full?(board)
+  empty_squares(board).empty?
+end
+
+def someone_won?(board)
+  !!detect_winner(board)
+end
+
+def detect_winner(board)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9]] + # rows
+                  [[1,4,7], [2,5,8], [3,6,9]] + # cols
+                  [[1,5,9], [3,5,7]]            # diagonals
+
+  winning_lines.each do |line|
+    if board[line[0]] == PLAYER_MARKER && 
+       board[line[1]] == PLAYER_MARKER && 
+       board[line[2]] == PLAYER_MARKER
+       return 'Player'
+     elsif board[line[0]] == COMPUTER_MARKER && 
+       board[line[1]] == COMPUTER_MARKER && 
+       board[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+     end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again? Y or N"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thanks for playing Tic Tac Toe! Goodbye!"
