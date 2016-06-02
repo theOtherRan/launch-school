144a9e63283432d3b6b1d0063ac922c74bb80a5b
theOtherRan <theotherran@gmail.com>
2016-06-01 17:36:46 -0800

    Use Rubocop to refactor and improve code

--- programming-and-back-end-development/Course-101-Programming-Foundations/Lesson-04-Slightly-Larger-Programs/twenty_one.rb
+++ programming-and-back-end-development/Course-101-Programming-Foundations/Lesson-04-Slightly-Larger-Programs/twenty_one.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
-SUITS = ['H', 'D', 'S', 'C']
-VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+SUITS = %w(H D S C).freeze
+VALUES = %w(2 3 4 5 6 7 8 9 10 J Q K A).freeze
 
 def prompt(message)
   puts "=> #{message}"
